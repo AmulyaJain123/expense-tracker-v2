7f8761897da032b18b781ef01e940f229f6dd39e
Amulya Jain <amulyajain123@gmail.com>
2024-12-21 00:38:35 +0530

    port modified

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ const main = async () => {
     try {
         await connectToDB();
         console.log("Connection Established")
-        server.listen(3000);
+        server.listen(process.env.PORT || 3000);
     }
     catch (error) {
         throw error;
